67040cdb079af25f2c56c11bbef77f6c36a135f2
1543-STACK <141954178+1543-STACK@users.noreply.github.com>
2024-02-02 10:48:29 +0200

    Added footer url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="Weather App"></div>;
+  return <div className="App">Weather App </div>;
 }
+<footer>
+  This project was coded by
+  <a href="https://github.com/1543-STACK/" target="_blank">
+    {" "}
+    Anu Govender
+  </a>
+  , is
+  <a href="https://github.com/1543-STACK/weather-react" target="_blank">
+    open-sourced on GitHub
+  </a>
+  and
+  <a href="https://anu weather.netlify.app/#/" target="_blank">
+    hosted on Netlify
+  </a>
+</footer>;
